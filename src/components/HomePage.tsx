a3aa6f4315791336e1613f9d967b5c90c2fda0cc
onlyoneaman <2000.aman.sinha@gmail.com>
2024-11-25 18:21:28 +0530

    Change Layout

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -120,7 +120,7 @@ const HomePage = () => {
 
       {/* Cards Grid */}
       <div className="w-full max-w-5xl mx-auto px-4 pb-9">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {designs
             // .filter(design => activeFilter === 'All' || design.category === activeFilter)
             .map((design, index) => (
